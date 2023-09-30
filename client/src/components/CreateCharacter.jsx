178f63b9923c89bcd4b267d01a5c3db3e47a482d
Leon Parsons <MY_NAME@example.com>
2023-09-30 14:13:50 +1300

    Adds dropdowns for all text fields

--- client/src/components/CreateCharacter.jsx
+++ client/src/components/CreateCharacter.jsx
@@ -6,9 +6,12 @@ function CreateCharacter() {
 
   const [charName, setCharName] = useState()
   const [charClass, setCharClass] = useState()
+  const [charRace, setCharRace] = useState()
+  const [charAlignment, setCharAlignment] = useState()
+  const [charBackground, setCharBackground] = useState()
 
   const Submit = () => {
-    axios.post('http://localhost:3001/createCharacter', {charName, charClass})
+    axios.post('http://localhost:3001/createCharacter', {charName, charClass, charRace, charAlignment, charBackground})
     .then((characters) => {
       console.log(characters)
     }).catch(err => console.log(err))
@@ -25,6 +28,30 @@ function CreateCharacter() {
         <option value = "Bard">Bard</option>
         <option value = "Cleric">Cleric</option>
         <option value = "Druid">Druid</option>
+      </select>
+
+      <label for="race-dd">Race</label>
+      <select name="race-dd" onChange={(e) => setCharRace(e.target.value)}>
+        <option value = "Elf">Elf</option>
+        <option value = "Orc">Orc</option>
+        <option value = "Human">Human</option>
+        <option value = "Tiefling">Tiefling</option>
+      </select>
+
+      <label for="alignment-dd">Alignment</label>
+      <select name="alignment-dd" onChange={(e) => setCharAlignment(e.target.value)}>
+        <option value = "Lawful-Good">Lawful-Good</option>
+        <option value = "Lawful-Neutral">Lawful-Neutral</option>
+        <option value = "Lawful-Evil">Lawful-Evil</option>
+        <option value = "True-Neutral">True-Neutral</option>
+      </select>
+
+      <label for="background-dd">Background</label>
+      <select name="background-dd" onChange={(e) => setCharBackground(e.target.value)}>
+        <option value = "Acolyte">Acolyte</option>
+        <option value = "Charlatan">Charlatan</option>
+        <option value = "Criminal">Criminal</option>
+        <option value = "Entertainer">Entertainer</option>
       </select>
 
       <button onClick={Submit}>Create Character</button>
