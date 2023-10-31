abfbe1a6d1ed549b102050db536e7ca2b96681cd
Leon Parsons <MY_NAME@example.com>
2023-10-31 18:14:42 +1300

    Refactors the display created characters page

--- client/src/pages/Chars.jsx
+++ client/src/pages/Chars.jsx
@@ -1,9 +1,12 @@
+import DisplayCharacters from "../components/DisplayCharacters";
+
 const Chars= () => {
   return (
     <>
-    <h1>Your Characters Page</h1>;
-  </>
-  )
+      <h1>Display Characters Page</h1>
+      <DisplayCharacters/>
+    </>
+ )
 };
 
 export default Chars;
